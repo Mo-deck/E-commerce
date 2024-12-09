07d0cced35f122c6b516deeea7253c3974bcc711
Mo-Deck <mohamedabow6774@gmail.com>
2024-12-09 10:55:24 +0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 function Footer() {
+
+  const currentYear = new Date().getFullYear();
+  
   return (
-    <div>Footer</div>
+    <div className="py-8 mt-16 border-t border-gray-200">
+      
+      <div className="max-w-4xl mx-auto text-center">
+        <p className="text-gray-600 font-light nb-2">Thanks for visiting this website</p>
+        <p className="text-gray-800 font-semibold">Dugsiiye &copy; 2023</p>
+      </div>
+    </div>
   )
 }
 
