e30f1c07ad2046ada6db36c6a19b2fd35b83c17b
Mo-Deck <mohamedabow6774@gmail.com>
2024-12-09 10:33:32 +0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 
 function App(){
 return  (
-    <div>
+    <div className="max-w-4xl mx-auto pt-16">
         <Header />
         {/* dynamic */}
         <Outlet />
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import react from 'react';
 
 const About = ()=>{
-    return <div>About</div>
+    return <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae eaque ab sint, facere amet corporis nesciunt inventore natus quo quidem alias facilis obcaecati eum, rerum cupiditate sapiente consectetur doloremque harum, similique blanditiis? Totam consectetur libero perferendis eum inventore tempore maiores enim numquam cumque praesentium ea odio facilis quia, dolores, esse ipsam iste, hic adipisci a qui officia quae ullam. Quasi voluptates recusandae ex sapiente commodi consequuntur voluptatibus, cumque incidunt minima explicabo dolores perspiciatis unde aspernatur sed nostrum praesentium laboriosam, sequi ducimus veniam? Dolor sint voluptate consequatur ut asperiores molestias. Consectetur architecto ab quaerat amet placeat rerum distinctio accusamus quibusdam dignissimos!</div>
 }
 
 export default About;
